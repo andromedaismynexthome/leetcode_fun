ba67127bf50aff19172b25dec7d1f3946572b07d
Dev <downlaodsnowinx2speed.com>
2024-01-23 12:20:38 +0530

    huffff

--- 26.Remove_Duplicates_from_Sorted_Array.py
+++ 26.Remove_Duplicates_from_Sorted_Array.py
@@ -47,12 +47,12 @@ nums is sorted in non-decreasing order.
 """
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        i=1
-        while i<len(nums):
-            if nums[i-1] == nums[i]:
-                nums.remove(nums[i-1])
-            i += 1
-        return len(nums), nums
+        j = 1
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+               nums[j] = nums[i]
+               j += 1
+        return j
 if __name__ == "__main__":
     obj = Solution()
     show = obj.removeDuplicates([1,1,2])
